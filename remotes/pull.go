6b4d52f3d9e9ca51ec20a0cdaa8fa1251a603651
Silvin Lubecki <silvin.lubecki@docker.com>
2019-08-09 17:11:39 +0200

    Refactor remotes/pull.go to split large functions into small ones
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- remotes/pull.go
+++ remotes/pull.go
@@ -33,6 +33,7 @@ func Pull(ctx context.Context, ref reference.Named, resolver remotes.Resolver) (
 
 func getIndex(ctx context.Context, ref auth.Scope, resolver remotes.Resolver) (ocischemav1.Index, error) {
 	logger := log.G(ctx)
+
 	logger.Debug("Getting OCI Index Descriptor")
 	resolvedRef, indexDescriptor, err := resolver.Resolve(withMutedContext(ctx), ref.String())
 	if err != nil {
@@ -58,36 +59,64 @@ func getIndex(ctx context.Context, ref auth.Scope, resolver remotes.Resolver) (o
 }
 
 func getConfig(ctx context.Context, ref opts.NamedOption, resolver remotes.Resolver, index ocischemav1.Index) (converter.BundleConfig, error) {
-	logger := log.G(ctx)
-	logger.Debug("Getting Bundle Config Manifest Descriptor")
-	// config is wrapped in an image manifest. So we first pull the manifest
-	// and then the config blob within it
-	configManifestDescriptor, err := converter.GetBundleConfigManifestDescriptor(&index)
-	if err != nil {
-		return converter.BundleConfig{}, fmt.Errorf("failed to get bundle config manifest from %q: %s", ref, err)
-	}
 	repoOnly, err := reference.ParseNormalizedNamed(ref.Name())
 	if err != nil {
 		return converter.BundleConfig{}, fmt.Errorf("invalid bundle config manifest reference name %q: %s", ref, err)
 	}
+
+	// config is wrapped in an image manifest. So we first pull the manifest
+	// and then the config blob within it
+	configManifestDescriptor, err := getConfigManifestDescriptor(ctx, ref, index)
+	if err != nil {
+		return converter.BundleConfig{}, err
+	}
+
+	manifest, err := getConfigManifest(ctx, ref, repoOnly, resolver, configManifestDescriptor)
+	if err != nil {
+		return converter.BundleConfig{}, err
+	}
+
+	// Pull now the config itself
+	return getBundleConfig(ctx, ref, repoOnly, resolver, manifest)
+}
+
+func getConfigManifestDescriptor(ctx context.Context, ref opts.NamedOption, index ocischemav1.Index) (ocischemav1.Descriptor, error) {
+	logger := log.G(ctx)
+
+	logger.Debug("Getting Bundle Config Manifest Descriptor")
+	configManifestDescriptor, err := converter.GetBundleConfigManifestDescriptor(&index)
+	if err != nil {
+		return ocischemav1.Descriptor{}, fmt.Errorf("failed to get bundle config manifest from %q: %s", ref, err)
+	}
 	logPayload(logger, configManifestDescriptor)
+
+	return configManifestDescriptor, nil
+}
+
+func getConfigManifest(ctx context.Context, ref opts.NamedOption, repoOnly reference.Named, resolver remotes.Resolver, configManifestDescriptor ocischemav1.Descriptor) (ocischemav1.Manifest, error) {
+	logger := log.G(ctx)
 
 	logger.Debugf("Getting Bundle Config Manifest %s", configManifestDescriptor.Digest)
 	configManifestRef, err := reference.WithDigest(repoOnly, configManifestDescriptor.Digest)
 	if err != nil {
-		return converter.BundleConfig{}, fmt.Errorf("invalid bundle config manifest reference name %q: %s", ref, err)
+		return ocischemav1.Manifest{}, fmt.Errorf("invalid bundle config manifest reference name %q: %s", ref, err)
 	}
 	configManifestPayload, err := pullPayload(ctx, resolver, configManifestRef.String(), configManifestDescriptor)
 	if err != nil {
-		return converter.BundleConfig{}, fmt.Errorf("failed to pull bundle config manifest %q: %s", ref, err)
+		return ocischemav1.Manifest{}, fmt.Errorf("failed to pull bundle config manifest %q: %s", ref, err)
 	}
 	var manifest ocischemav1.Manifest
 	if err := json.Unmarshal(configManifestPayload, &manifest); err != nil {
-		return converter.BundleConfig{}, err
+		return ocischemav1.Manifest{}, err
 	}
 	logPayload(logger, manifest)
 
-	// Pull now the config itself
+	return manifest, err
+}
+
+func getBundleConfig(ctx context.Context, ref opts.NamedOption, repoOnly reference.Named, resolver remotes.Resolver, manifest ocischemav1.Manifest) (converter.BundleConfig, error) {
+	logger := log.G(ctx)
+
 	logger.Debugf("Fetching Bundle Config %s", manifest.Config.Digest)
 	configRef, err := reference.WithDigest(repoOnly, manifest.Config.Digest)
 	if err != nil {
